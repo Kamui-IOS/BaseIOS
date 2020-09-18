45cc31c4c10bb3ef596c5b2884c52b95ac1e34e8
namhv@mobgame.vn <namhv@mobgame.vn>
2020-09-18 18:25:35 +0700

    logic bang loto

--- Loto/Loto/Controller/XoSo/CollectionViewCells/MienBacCollectionViewCell.swift
+++ Loto/Loto/Controller/XoSo/CollectionViewCells/MienBacCollectionViewCell.swift
@@ -146,13 +146,140 @@ class MienBacCollectionViewCell: UICollectionViewCell {
     func loto(dataLotoArray: [String]) {
         
         for str in dataLotoArray {
-            var dauduoi = DauDuoiLoto()
+            let dauduoi = DauDuoiLoto()
             dauduoi.getData(data: str)
             self.dauduoiArr.append(dauduoi)
         }
+        showData(lotoArr: self.dauduoiArr)
     }
     
     func showData(lotoArr: [DauDuoiLoto]) {
+        var dau0 = "", dau1 = "", dau2 = "", dau3 = "", dau4 = "",dau5 = "", dau6 = "", dau7 = "", dau8 = "", dau9 = ""
+        var duoi0 = "", duoi1 = "", duoi2 = "", duoi3 = "", duoi4 = "", duoi5 = "", duoi6 = "", duoi7 = "", duoi8 = "", duoi9 = ""
+        
+        for value in lotoArr {
+            if value.dau == "0" {
+                duoi0.append(value.duoi ?? "")
+                duoi0.append(", ")
+            }
+            if value.duoi == "0" {
+                dau0.append(value.dau ?? "")
+                dau0.append(", ")
+            }
+            
+            if value.dau == "1" {
+                duoi1.append(value.duoi ?? "")
+                duoi1.append(", ")
+            }
+            if value.duoi == "1" {
+                dau1.append(value.dau ?? "")
+                dau1.append(", ")
+            }
+            
+            if value.dau == "2" {
+                duoi2.append(value.duoi ?? "")
+                duoi2.append(", ")
+            }
+            if value.duoi == "2" {
+                dau0.append(value.dau ?? "")
+                dau0.append(", ")
+            }
+            
+            if value.dau == "3" {
+                duoi3.append(value.duoi ?? "")
+                duoi3.append(", ")
+            }
+            if value.duoi == "3" {
+                dau3.append(value.dau ?? "")
+                dau3.append(", ")
+            }
+            
+            if value.dau == "4" {
+                duoi4.append(value.duoi ?? "")
+                duoi4.append(", ")
+            }
+            if value.duoi == "4" {
+                dau4.append(value.dau ?? "")
+                dau4.append(", ")
+            }
+            
+            if value.dau == "5" {
+                duoi5.append(value.duoi ?? "")
+                duoi5.append(", ")
+            }
+            if value.duoi == "5" {
+                dau5.append(value.dau ?? "")
+                dau5.append(", ")
+            }
+            
+            if value.dau == "6" {
+                duoi6.append(value.duoi ?? "")
+                duoi6.append(", ")
+            }
+            if value.duoi == "6" {
+                dau6.append(value.dau ?? "")
+                dau6.append(", ")
+            }
+            
+            if value.dau == "7" {
+                duoi7.append(value.duoi ?? "")
+                duoi7.append(", ")
+            }
+            if value.duoi == "7" {
+                dau7.append(value.dau ?? "")
+                dau7.append(", ")
+            }
+            
+            if value.dau == "8" {
+                duoi8.append(value.duoi ?? "")
+                duoi8.append(", ")
+            }
+            if value.duoi == "8" {
+                dau8.append(value.dau ?? "")
+                dau8.append(", ")
+            }
+            
+            if value.dau == "9" {
+                duoi9.append(value.duoi ?? "")
+                duoi9.append(", ")
+            }
+            if value.duoi == "9" {
+                dau9.append(value.dau ?? "")
+                dau9.append(", ")
+            }
+        }
+        self.dau_0.text = dau0
+        self.duoi_0.text = duoi0
+        
+        self.dau_1.text = dau1
+        self.duoi_1.text = duoi1
+        
+        self.dau_2.text = dau2
+        self.duoi_2.text = duoi2
+        
+        self.dau_3.text = dau3
+        self.duoi_3.text = duoi3
+        
+        self.dau_4.text = dau4
+        self.duoi_4.text = duoi4
+        
+        self.dau_5.text = dau5
+        self.duoi_5.text = duoi5
+        
+        self.dau_6.text = dau6
+        self.duoi_6.text = duoi6
+        
+        self.dau_7.text = dau7
+        self.duoi_7.text = duoi7
+        
+        self.dau_7.text = dau7
+        self.duoi_7.text = duoi7
+        
+        self.dau_8.text = dau8
+        self.duoi_8.text = duoi8
+        
+        self.dau_9.text = dau9
+        self.duoi_9.text = duoi9
         
     }
 }
